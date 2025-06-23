f7ab16d3c9e0c74a2bab77002b29ee171b089eda
mdshaik851 <md939255shaik@gmail.com>
2025-06-23 17:31:43 +0530

    Modifed changes

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
-variable "trail_name"
-{
-default = "aws-login-trail"
-type = string
+variable "trail_name" {
+  default     = "aws-login-trail"
+  type        = string
+  description = "Name of the CloudTrail trail"
 }
 
-variable "s3_bucket_name"
-{
-default = "uc-14-consolelogintrail"
-type = string
+variable "s3_bucket_name" {
+  default     = "uc-14-consolelogintrail"
+  type        = string
+  description = "S3 bucket name to store CloudTrail logs"
 }
 
-variable "cloudwatch_logs_role_arn"
-{
-default = "arn:aws:iam::123456789012:role/CloudTrailCWLogsRole"
-type = string   
-}.
-
-variable "topic_name"
-{
-default = "aws-console-login-topic"
-type = string   
+variable "cloudwatch_logs_role_arn" {
+  default     = "arn:aws:iam::123456789012:role/CloudTrailCWLogsRole"
+  type        = string
+  description = "IAM role ARN for CloudTrail to send logs to CloudWatch"
 }
 
-variable "email_address"
-{
-default = "mohammedabdul.shaik@hcltech.com"
-type = string   
+variable "topic_name" {
+  default     = "aws-console-login-topic"
+  type        = string
+  description = "SNS topic name for login notifications"
+}
+
+variable "email_address" {
+  default     = "mohammedabdul.shaik@hcltech.com"
+  type        = string
+  description = "Email address to receive login notifications"
 }
