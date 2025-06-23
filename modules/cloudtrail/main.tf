dbb36831e875b2213bda453d98dc664d4dbf76bd
md939255shaik@gmail.com <mdshaik878@gmail.com>
2025-06-23 22:21:06 +0530

    modified changes

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,3 +1,42 @@
+resource "aws_s3_bucket" "cloudtrail_bucket" {
+  bucket = var.s3_bucket_name
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:GetBucketAcl",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.bucket}"
+      },
+      {
+        Sid       = "AWSCloudTrailWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.bucket}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_cloudtrail" "console_trail" {
   name                          = "console-login-trail"
   s3_bucket_name                = var.s3_bucket_name
@@ -5,11 +44,11 @@ resource "aws_cloudtrail" "console_trail" {
   is_multi_region_trail         = true
   enable_log_file_validation    = true
   cloud_watch_logs_role_arn     = var.cloudwatch_role_arn
-  cloud_watch_logs_group_arn    = "${var.log_group_arn}:*"
-#   is_logging                    = true
-  depends_on                    = [aws_s3_bucket.cloudtrail_bucket]
-}
+  cloud_watch_logs_group_arn    = var.log_group_arn
+  # is_logging                    = true
 
-resource "aws_s3_bucket" "cloudtrail_bucket" {
-  bucket = var.s3_bucket_name
+  depends_on = [
+    aws_s3_bucket.cloudtrail_bucket,
+    aws_s3_bucket_policy.cloudtrail_policy
+  ]
 }
