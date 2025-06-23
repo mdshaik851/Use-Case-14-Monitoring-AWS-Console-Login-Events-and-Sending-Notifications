d98987e3c6f5374bb1ba2b46d1ff492c612ee973
md939255shaik@gmail.com <mdshaik878@gmail.com>
2025-06-23 19:48:07 +0530

    Modified changes

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,4 +1,4 @@
-output "cloudtrail_logs_role_arm" {
+output "cloudtrail_logs_role_arn" {  # Fixed the typo here
   value       = aws_iam_role.cloudtrail_logs_role.arn
-  description = "ARN of the Cloud Trail"
+  description = "ARN of the CloudTrail CloudWatch Logs Role"
 }
