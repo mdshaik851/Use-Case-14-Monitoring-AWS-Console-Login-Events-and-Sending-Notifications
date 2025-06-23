121ec1dd6f708aa7cb27c9e03189f8d6b65da60a
md939255shaik@gmail.com <mdshaik878@gmail.com>
2025-06-23 22:50:47 +0530

    modified changes

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -43,8 +43,8 @@ resource "aws_cloudtrail" "console_trail" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_log_file_validation    = true
-  cloud_watch_logs_role_arn     = var.cloudwatch_role_arn
-  cloud_watch_logs_group_arn    = var.log_group_arn
+  cloud_watch_logs_role_arn     = "${var.cloudwatch_role_arn}:*"
+  cloud_watch_logs_group_arn    = "${var.log_group_arn}:*"
   # is_logging                    = true
 
   depends_on = [
