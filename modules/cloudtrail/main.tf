08a0e7f71668c47ae27701a4c00f418dee8b8a20
mdshaik851 <md939255shaik@gmail.com>
2025-06-23 18:07:33 +0530

    modifed changes

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -7,9 +7,10 @@ resource "aws_cloudtrail" "main" {
   cloud_watch_logs_group_arn    = var.cloudwatch_logs_group_arn
   cloud_watch_logs_role_arn     = var.cloudwatch_logs_role_arn
 
-  depends_on = [
-  aws_cloudwatch_log_group.cloudtrail_log_group,
-  aws_iam_role.cloudtrail_logs_role
+depends_on = [
+  var.cloudwatch_logs_group_arn,
+  var.cloudwatch_logs_role_arn
 ]
+
 
 }
